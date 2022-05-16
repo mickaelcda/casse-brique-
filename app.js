f012cf5b130a906719a70c83f204ffb3ac6e2ce8
mc.coquio <mc.coquio@gmail.com>
2022-05-16 20:03:59 +0200

    html css + canvas barre et balle

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const rayonBalle = 10, barreHauteur = 10, barreLargeur = 75;
+
+// Positionner la balle et la barre sur x et y 
+let x = canvas.width/2, y = canvas.height -30,
+barreX = (canvas.width - barreLargeur)/2;
+
+// Dessiner la barre et la balle 
+
+function dessineBalle() {
+    
+    ctx.beginPath();
+    ctx.arc(x, y, rayonBalle, 0, Math.PI * 2);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+    ctx.closePath();
+}
+//dessineBalle()
+
+function dessinebarre() {
+
+    ctx.beginPath();
+    ctx.rect(barreX, canvas.height - barreHauteur - 2, barreLargeur, barreHauteur); //position sur x puis y  longueur et hauteur
+    ctxfillStyle = '#333'; 
+    ctx.fill();
+    ctx.closePath()
+}
+
+//dessinebarre()
 
